6afe04383b21e1763c711fc466dc5ed0c183c2a1
Ben Batschelet <batschel@adobe.com>
2018-12-17 16:07:43 -0600

    Initial Resolver Factory

---
+++ test/ResolverFactoryTest.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magento\Upward\Test;
+
+use Magento\Upward\Definition;
+use Magento\Upward\ResolverFactory;
+use Magento\Upward\Resolver\ResolverInterface;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use function BeBat\Verify\verify;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class ResolverFactoryTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testGetForDefinitionWithResolver(): void
+    {
+        $resolver = Mockery::mock(ResolverInterface::class);
+        $defininition = new Definition(['resolver' => 'test']);
+
+        $resolver->shouldReceive('isValid')
+            ->with($defininition)
+            ->andReturn(true);
+
+        ResolverFactory::addResolver('test', $resolver);
+
+        verify(ResolverFactory::get($defininition))->is()->sameAs($resolver);
+    }
+
+    public function testGetForDefinitionWithInference(): void
+    {
+        $resolver = Mockery::mock(ResolverInterface::class);
+        $defininition = new Definition(['someKey' => 'someValue']);
+
+        $resolver->shouldReceive('getIndicator')
+            ->andReturn('someKey');
+        $resolver->shouldReceive('isValid')
+            ->with($defininition)
+            ->andReturn(true);
+
+        // Add a "dummy" test resolver type
+        ResolverFactory::addResolverClass('test', 'stdClass');
+        ResolverFactory::addResolver('test', $resolver);
+
+        verify(ResolverFactory::get($defininition))->is()->sameAs($resolver);
+    }
+
+    public function testGetForScalarShorthand(): void
+    {
+        $resolver = Mockery::mock(ResolverInterface::class);
+
+        $resolver->shouldReceive('isShorthand')
+            ->with('scalar value')
+            ->andReturn(true);
+
+        // Add a "dummy" test resolver that points to PHP stdClass
+        ResolverFactory::addResolverClass('test', 'stdClass');
+        ResolverFactory::addResolver('test', $resolver);
+
+        verify(ResolverFactory::get('scalar value'))->is()->sameAs($resolver);
+    }
+
+    public function testInvalidDefinition(): void
+    {
+        $resolver = Mockery::mock(ResolverInterface::class);
+        $defininition = new Definition(['resolver' => 'test']);
+
+        $resolver->shouldReceive('isValid')
+            ->with($defininition)
+            ->andReturn(false);
+
+        ResolverFactory::addResolver('test', $resolver);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Definition {"resolver":"test"} is not valid for'); // omit mock class name
+
+        ResolverFactory::get($defininition);
+    }
+
+    public function testGetForScalarNoResolver(): void
+    {
+        $resolver = Mockery::mock(ResolverInterface::class);
+
+        $resolver->shouldReceive('isShorthand')
+            ->with('scalar value')
+            ->andReturn(false);
+
+        ResolverFactory::addResolver('test', $resolver);
+
+        verify(ResolverFactory::get('scalar value'))->is()->null();
+    }
+
+    public function testNoResolverClassDefined(): void
+    {
+        $defininition = new Definition(['resolver' => 'no-resolver']);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('No resolver class defined for resolver no-resolver');
+
+        ResolverFactory::get($defininition);
+    }
+
+    public function testNoResovlerInferred(): void
+    {
+        $defininition = new Definition([]);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('No resolver found for definition');
+
+        ResolverFactory::get($defininition);
+    }
+}
