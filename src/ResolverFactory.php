8af9ecd30b9a417578673ca012db88ea5ab14add
Ben Batschelet <batschel@adobe.com>
2019-01-16 11:49:23 -0600

    Conditional Resolver

--- src/ResolverFactory.php
+++ src/ResolverFactory.php
@@ -22,11 +22,12 @@ class ResolverFactory
      * @var array map of resolver key to their class implementation
      */
     private static $resolverClasses = [
-        self::RESOLVER_TYPE_DIRECTORY => Resolver\Directory::class,
-        self::RESOLVER_TYPE_FILE      => Resolver\File::class,
-        self::RESOLVER_TYPE_INLINE    => Resolver\Inline::class,
-        self::RESOLVER_TYPE_PROXY     => Resolver\Proxy::class,
-        self::RESOLVER_TYPE_TEMPLATE  => Resolver\Template::class,
+        self::RESOLVER_TYPE_CONDITIONAL => Resolver\Conditional::class,
+        self::RESOLVER_TYPE_DIRECTORY   => Resolver\Directory::class,
+        self::RESOLVER_TYPE_FILE        => Resolver\File::class,
+        self::RESOLVER_TYPE_INLINE      => Resolver\Inline::class,
+        self::RESOLVER_TYPE_PROXY       => Resolver\Proxy::class,
+        self::RESOLVER_TYPE_TEMPLATE    => Resolver\Template::class,
     ];
 
     /**
